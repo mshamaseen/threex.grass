84ce592e80a52b97ace4c5828de2ff4646b5a0e8
shamaseen <m.shamaseen@outlook.com>
2020-04-11 01:36:46 +0300

    upgrade it to run on three js 112 + make a random grass scale

--- threex.grass.js
+++ threex.grass.js
@@ -1,9 +1,21 @@
-var THREEx	= THREEx	|| {}
+var THREEx	= THREEx	|| {};
 
 THREEx.createGrassTufts	= function(positions){
+
+    // load the texture
+    var textureUrl	= THREEx.createGrassTufts.baseUrl+'images/grass01.png';
+    var texture	= new THREE.TextureLoader().load(textureUrl);
+    // build the material
+    var material	= new THREE.MeshPhongMaterial({
+        map		: texture,
+        color		: 'grey',
+        emissive	: 'darkgreen',
+        alphaTest	: 0.7,
+    });
+
 	// create the initial geometry
-	var geometry	= new THREE.PlaneGeometry(0.4, 0.2)
-	geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, geometry.height/2, 0 ) );
+	var geometry	= new THREE.PlaneGeometry(1,1);
+	// geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, geometry.height/2, 0 ) );
 
 
 	// Tweat the normal for better lighting
@@ -13,50 +25,48 @@ THREEx.createGrassTufts	= function(positions){
 		face.vertexNormals.forEach(function(normal){
 			normal.set(0.0,1.0,0.0).normalize()
 		})
-	})
-	
+	});
+
 	// create each tuft and merge their geometry for performance
 	var mergedGeo	= new THREE.Geometry();
 	for(var i = 0; i < positions.length; i++){
-		var position	= positions[i]			
-		var baseAngle	= Math.PI*2*Math.random()
+		var position	= positions[i];
+		var baseAngle	= Math.PI*2*Math.random();
+        let randomScale = (Math.floor(Math.random() * (10 - 5) ) + 5) / 10;
 
-		var nPlanes	= 2
+		var nPlanes	= 2;
 		for(var j = 0; j < nPlanes; j++){
-			var angle	= baseAngle+j*Math.PI/nPlanes
+			var angle	= baseAngle+j*Math.PI/nPlanes;
+
 
 			// First plane
-			var object3d	= new THREE.Mesh(geometry, material)
-			object3d.rotateY(angle)
-			object3d.position.copy(position)
-			THREE.GeometryUtils.merge( mergedGeo, object3d );
+			var object3d	= new THREE.Mesh(geometry, material);
+            object3d.scale.set(randomScale,randomScale,1);
+			object3d.rotateY(angle);
+			object3d.position.copy(position);
+            object3d.position.y = randomScale / 2;
+            mergedGeo.mergeMesh( object3d );
 
 			// The other side of the plane
-			// - impossible to use ```side : THREE.BothSide``` as 
+			// - impossible to use ```side : THREE.BothSide``` as
 			//   it would mess up the normals
-			var object3d	= new THREE.Mesh(geometry, material)
-			object3d.rotateY(angle+Math.PI)
-			object3d.position.copy(position)
-			THREE.GeometryUtils.merge( mergedGeo, object3d );
+			let object3d2	= new THREE.Mesh(geometry, material);
+            object3d2.scale.set(randomScale,randomScale,1);
+			object3d2.rotateY(angle+Math.PI);
+			object3d2.position.copy(position);
+            object3d2.position.y = randomScale / 2;
+
+
+            mergedGeo.mergeMesh(  object3d2 );
 		}
 	}
 
-
-	// load the texture
-	var textureUrl	= THREEx.createGrassTufts.baseUrl+'images/grass01.png'
-	var texture	= THREE.ImageUtils.loadTexture(textureUrl)
-	// build the material
-	var material	= new THREE.MeshPhongMaterial({
-		map		: texture,
-		color		: 'grey',
-		emissive	: 'darkgreen',
-		alphaTest	: 0.7,
-	})
 	// create the mesh
-	var mesh	= new THREE.Mesh(mergedGeo, material)
-	return mesh	
-}
+    let mesh = new THREE.Mesh(mergedGeo, material);
+	mesh.name = "grass";
+	return mesh;
+};
 
-THREEx.createGrassTufts.baseUrl	= "../"
+THREEx.createGrassTufts.baseUrl	= "../";
 
 
